da366803c2b68f8fb0a9e5e75fdb31e07de0f7cc
Muhammed Basheer <basheerkomassery@gmail.com>
2023-05-23 15:39:59 +0530

    created views and serializers to render users and create users and configured urls

--- user_cred_api/profiles_api/views.py
+++ user_cred_api/profiles_api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from profiles_api import models
+from profiles_api import serializers
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
